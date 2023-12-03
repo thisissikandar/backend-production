f846b02bf220e89f7b9a39f8f347ce9fff14a1a2
thisissikandar <sikandarc989@gmail.com>
2023-12-03 14:44:02 +0530

    routes declarations complete

--- src/app.js
+++ src/app.js
@@ -8,11 +8,18 @@ app.use(
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
-);
+  );
+  
+  app.use(express.json({ limit: "15kb" }));
+  app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+  app.use(express.static("public"));
+  app.use(cookieParser());
+  
+  //routes import
+import userRouter from './routes/user.routes.js'
 
-app.use(express.json({ limit: "15kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-app.use(cookieParser);
-
+//routes declaration
+app.use("/api/v1/users", userRouter)
+  
+  
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
@@ -10,13 +11,13 @@ connectDb()
       throw err;
     });
     app.listen(process.env.APP_PORT || 3000, () => {
-      console.log(`App Running on http`);
+      console.log(`App Running on http://localhost:${process.env.APP_PORT}`);
     });
   })
   .catch((err) => {
     console.log("MONGO DB Connection Failed:", err);
   });
-const app = express();
+
 
 // (async()=>{
 //     try {
