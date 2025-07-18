96d55ebd390aebdc86a47de34061de725e38f762
Sikandar Chauhan <sikandarc989@gmail.com>
2025-07-18 16:33:23 +0530

    feat: add controllers and routes for comments, likes, playlists, subscriptions, tweets, and videos; update app.js for new routes

--- src/app.js
+++ src/app.js
@@ -8,19 +8,32 @@ app.use(
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
-  );
-  
-  app.use(express.json({ limit: "15kb" }));
-  app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-  app.use(express.static("public"));
-  app.use(cookieParser());
-  
-  //routes import
-import userRouter from './routes/user.routes.js'
+);
+
+app.use(express.json({ limit: "15kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 import { errorHandler } from "./middlewares/error.middlewalre.js";
 
 //routes declaration
-app.use("/api/v1/users", userRouter)
-  
-app.use(errorHandler)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use(errorHandler);
 export { app };
---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,27 @@
+import mongoose, { isValidObjectId } from "mongoose"
+
+import { asyncHandler } from "../utils/asynHandler.js"
+
+
+const createTweet = asyncHandler(async (req, res) => {
+    //TODO: create tweet
+})
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+})
+
+const updateTweet = asyncHandler(async (req, res) => {
+    //TODO: update tweet
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    //TODO: delete tweet
+})
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+}
